00a3482f7304d9528aa19e71eaa4d8d23d4e0db1
Clems-web <clement.servais1@gmail.com>
2020-12-01 21:02:29 +0100

    finito

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ let para4 = document.getElementById("random");
 let para5 = document.getElementById("trunc");
 let para6 = document.getElementById("replace");
 let operande = 10;
-let numberEntier = parseInt(para1);
+let numberEntier = parseInt(para1.innerHTML);
+let numberFlottant = parseFloat(para2.innerHTML);
 
-para1.innerText = numberEntier*operande;
+para1.innerHTML = numberEntier*operande;
+para2.innerHTML = numberFlottant*operande + parseInt(para1.innerHTML);
 
+if (isNaN(para3.innerHTML)) {
+    para3.innerHTML = 0;
+}
+
+para4.innerHTML = Math.trunc((Math.random()*100));
+
+para5.innerHTML = Math.trunc(para5.innerHTML);
+
+para6.innerHTML = para6.innerHTML.replace("world", "Clement Servais");

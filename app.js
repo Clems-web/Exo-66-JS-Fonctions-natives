5b80f8fbfc6d7c3ec267299edd6497ee973678dd
Clems-web <clement.servais1@gmail.com>
2020-12-01 16:50:57 +0100

    fatigue.exe

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log(random());
+let para1 = document.getElementById("entier");
+let para2 = document.getElementById("flottant");
+let para3 = document.getElementById("non-numerique");
+let para4 = document.getElementById("random");
+let para5 = document.getElementById("trunc");
+let para6 = document.getElementById("replace");
+let operande = 10;
+let numberEntier = parseInt(para1);
+
+para1.innerText = numberEntier*operande;
+
